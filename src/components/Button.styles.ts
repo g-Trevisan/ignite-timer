39d37ad68a0195bc8268434ae612970853787124
g-Trevisan <gtrevis15@gmail.com>
2024-09-01 16:49:02 -0300

    eslint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
     variant: ButtonVariant;
