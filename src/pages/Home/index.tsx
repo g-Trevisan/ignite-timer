0a4c1a45949a31aba6de2b48f38002ea3fec1406
g-Trevisan <gtrevis15@gmail.com>
2024-09-23 09:16:59 -0300

    controlled vs uncontrolled

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,14 +8,30 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
+import { useState } from "react";
+
 
 export function Home() {
+
+  const [task, setTask] = useState('')
+
+  function resetForm(){
+    setTask('');
+  }
+
+
   return (
     <HomeContainer>
       <form action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput 
+            id="task" 
+            list="task-suggestions" 
+            placeholder="Dê um nome para o seu projeto" 
+            // onChange={(e) => setTask(e.target.value)}
+            // value={task}
+          />
 
           <datalist id="task-suggestions">
             <option value="Projeto 1"></option>
@@ -45,7 +61,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
